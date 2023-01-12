1188a3afe2fa7210fe5b3a5cb1659cc49dc3116a
Bryan Olson <hikumealan@hotmail.com>
2023-01-12 23:23:53 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  // res.send('Hello World!');
+  res.sendFile(path.join(__dirname, `/index.html`));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
